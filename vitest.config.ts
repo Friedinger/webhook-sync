4c033e7686a4d22c77dbcdf969bac60db2676bce
Friedinger <71267611+Friedinger@users.noreply.github.com>
2026-03-18 08:14:45 +0100

    ✨ Setup typescript

---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    coverage: {
+      provider: "v8",
+      enabled: true,
+      exclude: ["test/**", "package.json"],
+      thresholds: {
+        branches: 100,
+        functions: 100,
+        lines: 100,
+        statements: 100,
+        perFile: true,
+      },
+    },
+  },
+});
